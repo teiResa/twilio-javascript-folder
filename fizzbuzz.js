d6fc810e3ce177bbc8d397ae3daffac639939edb
teiResa <49374955+teiResa@users.noreply.github.com>
2021-04-17 19:04:36 +0800

    Create fizzbuzz.js

---
+++ fizzbuzz.js
@@ -0,0 +1,21 @@
+/* 
+Fizzbuzz is a program used to demonstrate basic functionality of a programming language.
+
+The Fizzbuzz program is to go through numbers 1 through 100. Printing the following:
+"fizz" for numbers divisible by 3;
+"buzz" for divisibles of 5s;
+and "fizzbuzz" (fizz + buzz) of both.
+
+print 👽 and the number if it is divisible by 3
+print 🚀 and the number if it is divisible by 5
+print 🛰 and the number if it is divisible by both 3 and 5
+print 💩 and the number if it is not divisible by either 3 or 5
+
+In JS, Comments are written with // for inlines, and /* multiliners * /.
+// for programs over 200 lines of code.
+Like a mix of C and CSS, ahahaha.
+
+
+
+
+*/
